178183e0644bd266d6528ca6cdc230d0a1096eba
Joao Vitor Teixeira <joao.teixeira@bemobi.com.br>
2021-01-11 16:16:19 -0300

    more tests

--- arrays_slices/array.go
+++ arrays_slices/array.go
@@ -6,6 +6,28 @@ func Sum(numbers []int) int{
 		sum += number
 	}
 	return sum
-	
-
 }
+
+func SumAll(numberstoSum ...[]int) [] int{
+	var sums []int
+	for _,  numbers := range numberstoSum {
+		sums = append(sums,Sum(numbers))
+	}
+	return sums
+}
+
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum{
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		}else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return sums
+}
+
+
