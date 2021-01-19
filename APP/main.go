53222887c73fb795c6c84af746f14eddca3e78bb
joaovgatti <joaovsgt@dcc.ufrj.br>
2021-01-19 16:50:03 -0300

    refactor

--- APP/main.go
+++ APP/main.go
@@ -6,9 +6,10 @@ import (
 )
 
 func main(){
-	server := &PlayerServer{}
+	server := &PlayerServer{NewInMemoryPlayerStore()}
 
 	if err := http.ListenAndServe(":5000", server); err != nil {
 		log.Fatalf("could not listen on port 5000 %v", err)
 	}
+
 }
